6a92a5d4d61ab7412a217a07e6923e9f6eb4ec0e
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-17 13:33:54 +0100

    Fetch products on DashboardContainer mount

--- src/components/dashboard/DashboardContainer.js
+++ src/components/dashboard/DashboardContainer.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { getProducts } from '../../actions/Product';
 
 import DashboardComponent from './DashboardComponent';
 
 class DashboardContainer extends Component {
+  componentWillMount() {
+    this.props.getProducts();
+  }
+
   render() {
     return (
       <DashboardComponent />
@@ -12,4 +20,12 @@ class DashboardContainer extends Component {
 
 DashboardContainer.route = '/';
 
-export default DashboardContainer;
+DashboardContainer.propTypes = {
+  product: PropTypes.object.isRequired,
+  getProducts: PropTypes.func.isRequired,
+};
+
+export default connect(
+  state => ({ product: state.product }),
+  dispatch => ({ getProducts: () => dispatch(getProducts()) })
+)(DashboardContainer);
