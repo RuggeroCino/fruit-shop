136ce0a19ef2e9366901be500a2810740a5cafe4
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-17 12:44:44 +0100

    Add actions and reducers to fetch products

---
+++ src/actions/Product.js
@@ -0,0 +1,8 @@
+import { fruitShopService } from '../../services/FruitShopService';
+
+export const getProducts = () => dispatch => {
+  dispatch({ type: 'product.getProductsRequest' });
+  fruitShopService.getProducts()
+    .then(res => dispatch({ type: 'product.getProductsSuccess', payload: res }))
+    .catch(err => dispatch({ type: 'product.getProductsFailure', payload: err }));
+};
--- src/reducers/Products.js
+++ src/reducers/Products.js
@@ -1,7 +1,30 @@
-const defaultState = { };
+const defaultState = {
+  isFetchingProducts: false,
+  products: [],
+  getProductsError: null,
+};
 
 export const Products = (state = defaultState, action) => {
+  let payload = action.payload;
+
   switch (action.type) {
+    case 'product.getProductsRequest':
+      return {
+        ...state,
+        isFetchingProducts: true,
+      };
+    case 'product.getProductsSuccess':
+      return {
+        ...state,
+        products: payload,
+        isFetchingProducts: false,
+      };
+    case 'product.getProductsFailure':
+      return {
+        ...state,
+        getProductsError: payload,
+        isFetchingProducts: false,
+      };
     default:
       return state;
   }
