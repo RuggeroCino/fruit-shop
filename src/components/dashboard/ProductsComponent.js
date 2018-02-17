9674f488dd1ef2e49ad3cc99ad5ddd6ea85e4bdf
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-17 14:33:27 +0100

    Add product card to ProductsComponent

--- src/components/dashboard/ProductsComponent.js
+++ src/components/dashboard/ProductsComponent.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'material-ui/Card';
+
+import FlatButton from 'material-ui/FlatButton';
 
 import FlexContainer from '../../shared/FlexContainer';
-import BodyText from '../../shared/BodyText';
 
 const ProductsComponent = (props) => {
   return (
     <FlexContainer
-      direction="column"
+      direction="row"
+      wrap="wrap"
       grow={1}
+      style={{maxWidth: 800}}
     >
       {props.products.map(product => (
-        <BodyText key={product.id}>{product.name}</BodyText>
+        <Card key={product.id} style={{flexGrow: 1, flexBasis: '40%', margin: 15}}>
+        <CardMedia>
+          <img
+            src={product.image}
+            alt="Product"
+            style={{maxWidth: 200}}
+          />
+        </CardMedia>
+        <CardTitle
+          title={product.name}
+          subtitle={product.type}
+        />
+        <CardText>
+          {product.description}
+        </CardText>
+        <CardActions>
+          <FlatButton label="Add" />
+          <FlatButton label="Remove" />
+        </CardActions>
+      </Card>
       ))
       }
     </FlexContainer>
