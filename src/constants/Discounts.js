6158a52ed7e610a320a3d6f3106935c8e531a7ef
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-19 18:07:25 +0100

    Add function to calculate discount price

--- src/constants/Discounts.js
+++ src/constants/Discounts.js
@@ -1,3 +1,6 @@
 export const Discounts = {
-  THREE_FOR_TWO: 'Three for the price of two',
+  THREE_FOR_TWO: {
+    description: 'Three for the price of two',
+    calculate: (price, quantity) => (quantity - (parseInt(quantity / 3, 10))) * price,
+  },
 };
