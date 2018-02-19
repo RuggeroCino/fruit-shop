6d5af6cd03860cf9873390f5f4c1fade6a3d70a1
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-19 14:48:55 +0100

    Add getBasketProducts function

--- src/reducers/Reducers.js
+++ src/reducers/Reducers.js
@@ -1,4 +1,7 @@
-import { products } from './products';
+import {
+  products,
+  getProductById,
+} from './products';
 import { basket } from './basket';
 import { notification } from './notification';
 
@@ -6,4 +9,13 @@ export const reducers = {
   products,
   basket,
   notification,
+};
+
+export const getBasketProducts = (state) => {
+  return state.basket.productIds.map(productId => {
+    return {
+      ...getProductById(state.products, productId),
+      quantity: state.basket.items[productId],
+    };
+  });
 };
