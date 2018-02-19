82e2372f57f56c7c40e8d3fed1f0e08217ce6c66
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-19 14:49:49 +0100

    Retrieve basket data on BasketContainer

--- src/components/basket/BasketComponent.js
+++ src/components/basket/BasketComponent.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import FlexContainer from '../../shared/FlexContainer';
 import Header from '../../shared/Header';
 import BodyText from '../../shared/BodyText';
 
-const BasketComponent = () => {
+const BasketComponent = (props) => {
   return (
     <FlexContainer
       direction="column"
       grow={1}
+      style={{marginBottom: 40}}
     >
-      <Header />
-      <BodyText>Basket</BodyText>
+      <Header totalBasketItems={props.totalBasketItems}/>
+      <FlexContainer
+        padding={true}
+        maxWidth={true}
+        direction="column"
+        align="center"
+        style={{width: '100%'}}
+      >
+        <BodyText title={true}>
+          Your basket
+        </BodyText>
+      </FlexContainer>
     </FlexContainer>
   );
+};
+
+BasketComponent.propTypes = {
+  totalBasketItems: PropTypes.number.isRequired,
+  basketProducts: PropTypes.array.isRequired,
 };
 
 export default BasketComponent;
--- src/components/basket/BasketContainer.js
+++ src/components/basket/BasketContainer.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { getTotalBasketItems } from '../../reducers/basket';
+import { getBasketProducts } from '../../reducers/reducers';
 
 import BasketComponent from './BasketComponent';
 
 class BasketContainer extends Component {
   render() {
     return (
-      <BasketComponent />
+      <BasketComponent
+        totalBasketItems={this.props.totalBasketItems}
+        basketProducts={this.props.basketProducts}
+      />
     );
   }
 }
 
 BasketContainer.route = '/basket';
 
-export default BasketContainer;
+BasketContainer.propTypes = {
+  totalBasketItems: PropTypes.number.isRequired,
+  basketProducts: PropTypes.array.isRequired,
+};
+
+export default connect(
+  state => ({
+    products: state.products,
+    basketProducts: getBasketProducts(state),
+    totalBasketItems: getTotalBasketItems(state.basket),
+  })
+)(BasketContainer);
