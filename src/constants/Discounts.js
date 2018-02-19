653e8900a40b9d1d9e19faf5429f892208325024
Ruggero Cino <ruggero.cino@ti8m.ch>
2018-02-19 18:19:18 +0100

    Format Discount constant

--- src/constants/Discounts.js
+++ src/constants/Discounts.js
@@ -1,6 +1,8 @@
 export const Discounts = {
   THREE_FOR_TWO: {
     description: 'Three for the price of two',
-    calculate: (price, quantity) => (quantity - (parseInt(quantity / 3, 10))) * price,
+    calculate: (price, quantity) => {
+      return (quantity - (parseInt(quantity / 3, 10))) * price;
+    },
   },
 };
